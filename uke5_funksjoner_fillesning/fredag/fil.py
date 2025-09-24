7e2b1743aa54b9f5dd9294567006de371d3e260e
Jonas Berger Nyvold <jonas.nyvold02@gmail.com>
2025-09-24 16:02:01 +0200

    week 6 list and dict tricks

--- uke5_funksjoner_fillesning/fredag/fil.py
+++ uke5_funksjoner_fillesning/fredag/fil.py
@@ -1,18 +1,19 @@
 
 # antar listene er definert utfor funksjon
-navn = []
-linjer = []
-trinn = []
+ordbok = {}
+
 
 def lesing(filsti):
     with open(filsti) as fil:
         for linje in fil:
-            linje = linje.split()
-            navn.append(linje[0])
-            linjer.append(linje[1])
-            trinn.append(linje[2])
+            linje = linje.split()#[navn, linje, trinn]
+            ordbok[linje[0]] = linje[1]
 
 lesing("studenter.txt")
-print(navn)
-print(linjer)
-print(trinn)
+print(ordbok)
+for fornavn, linje in ordbok.items():
+    if fornavn[0] == "a" or fornavn[0] == "A":
+        print(fornavn)
+        print(linje)
+        print("------")
+    
